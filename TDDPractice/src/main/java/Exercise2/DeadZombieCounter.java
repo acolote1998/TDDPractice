ef4f3eac4167d55026b55de86878adff3d2a7125
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-04 19:30:27 +0100

    passing test of zombieCounterStarts at zero

--- TDDPractice/src/main/java/Exercise2/DeadZombieCounter.java
+++ TDDPractice/src/main/java/Exercise2/DeadZombieCounter.java
@@ -1,4 +1,13 @@
 package Exercise2;
 
 public class DeadZombieCounter {
+    public int getCount() {
+        return 0;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    private int count;
 }
