bdb51edd8614cddf803fca9a533d06c86736b5ce
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-04 19:43:06 +0100

    Exercise 3 initialization - Life Counter

---
+++ TDDPractice/src/main/java/Exercise3/LifeCounter.java
@@ -0,0 +1,16 @@
+package Exercise3;
+
+/*
+
+Nuevo ejercicio: Contador de Vidas 🧟‍♂️
+Reglas:
+Tenemos un contador de "vidas" (LifeCounter). Vamos a hacer lo siguiente:
+
+El contador de vidas empieza en 3.
+Cada vez que se llama a takeLife(), se resta una vida (pero no puede quedar por debajo de 0).
+Queremos asegurarnos de que el contador nunca baje de 0, aunque sigamos llamando a takeLife().
+
+ */
+
+public class LifeCounter {
+}
---
+++ TDDPractice/src/test/java/Exercise3/LifeCounterTest.java
@@ -0,0 +1,19 @@
+package Exercise3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+
+Nuevo ejercicio: Contador de Vidas 🧟‍♂️
+Reglas:
+Tenemos un contador de "vidas" (LifeCounter). Vamos a hacer lo siguiente:
+
+El contador de vidas empieza en 3.
+Cada vez que se llama a takeLife(), se resta una vida (pero no puede quedar por debajo de 0).
+Queremos asegurarnos de que el contador nunca baje de 0, aunque sigamos llamando a takeLife().
+
+ */
+
+class LifeCounterTest {
+
+}
